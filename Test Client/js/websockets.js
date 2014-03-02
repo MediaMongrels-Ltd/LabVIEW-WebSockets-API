63b02101db1aacd39662e7c07e3ed8b73fb88a02
Sam Sharp <sam@mediamongrels.com>
2014-03-02 17:49:33 +0000

    - committing work on first version of WebSockets library

---
+++ Test Client/js/websockets.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {  
+  
+  if(!("WebSocket" in window)){  
+  $('#chatLog, input, button, #examples').fadeOut("fast");  
+  $('<p>Oh no, you need a browser that supports WebSockets. How about <a href="http://www.google.com/chrome">Google Chrome</a>?</p>').appendTo('#container');  
+  }else{  
+      //The user has WebSockets  
+  
+      connect();  
+  
+      function connect(){  
+          var socket;  
+          var host = "ws://192.168.0.6:6123";  
+  
+          try{  
+              var socket = new WebSocket(host);  
+  
+              message('<p class="event">Socket Status: '+socket.readyState);  
+  
+              socket.onopen = function(){  
+                 message('<p class="event">Socket Status: '+socket.readyState+' (open)');  
+              }  
+  
+              socket.onmessage = function(msg){  
+                 message('<p class="message">Received: '+msg.data);  
+              }  
+  
+              socket.onclose = function(){  
+                message('<p class="event">Socket Status: '+socket.readyState+' (Closed)');  
+              }           
+  
+          } catch(exception){  
+             message('<p>Error'+exception);  
+          }  
+  
+          function send(){  
+              var text = $('#text').val();  
+  
+              if(text==""){  
+                  message('<p class="warning">Please enter a message');  
+                  return ;  
+              }  
+              try{  
+                  socket.send(text);  
+                  message('<p class="event">Sent: '+text)  
+  
+              } catch(exception){  
+                 message('<p class="warning">');  
+              }  
+              $('#text').val("");  
+          }  
+  
+          function message(msg){  
+            $('#chatLog').append(msg+'</p>');  
+          }  
+  
+          $('#text').keypress(function(event) {  
+              if (event.keyCode == '13') {  
+                send();  
+              }  
+          });     
+  
+          $('#disconnect').click(function(){  
+             socket.close();  
+          });  
+  
+      }//End connect  
+  
+  }//End else  
+  
+}); 
---
+++ WebSockets.lvproj
@@ -0,0 +1,27 @@
+﻿<?xml version='1.0' encoding='UTF-8'?>
+<Project Type="Project" LVVersion="13008000">
+	<Item Name="My Computer" Type="My Computer">
+		<Property Name="server.app.propertiesEnabled" Type="Bool">true</Property>
+		<Property Name="server.control.propertiesEnabled" Type="Bool">true</Property>
+		<Property Name="server.tcp.enabled" Type="Bool">false</Property>
+		<Property Name="server.tcp.port" Type="Int">0</Property>
+		<Property Name="server.tcp.serviceName" Type="Str">My Computer/VI Server</Property>
+		<Property Name="server.tcp.serviceName.default" Type="Str">My Computer/VI Server</Property>
+		<Property Name="server.vi.callsEnabled" Type="Bool">true</Property>
+		<Property Name="server.vi.propertiesEnabled" Type="Bool">true</Property>
+		<Property Name="specify.custom.address" Type="Bool">false</Property>
+		<Item Name="Test Server" Type="Folder">
+			<Item Name="Test Server.vi" Type="VI" URL="../Test Server/Test Server.vi"/>
+		</Item>
+		<Item Name="Message Queue.lvlib" Type="Library" URL="../Message Queue/Message Queue.lvlib"/>
+		<Item Name="ReadMessage.ctl" Type="VI" URL="../WebSockets/ReadMessage.ctl"/>
+		<Item Name="WebSockets.lvlib" Type="Library" URL="../WebSockets/WebSockets.lvlib"/>
+		<Item Name="Dependencies" Type="Dependencies">
+			<Item Name="vi.lib" Type="Folder">
+				<Item Name="Clear Errors.vi" Type="VI" URL="/&lt;vilib&gt;/Utility/error.llb/Clear Errors.vi"/>
+				<Item Name="Error Cluster From Error Code.vi" Type="VI" URL="/&lt;vilib&gt;/Utility/error.llb/Error Cluster From Error Code.vi"/>
+			</Item>
+		</Item>
+		<Item Name="Build Specifications" Type="Build"/>
+	</Item>
+</Project>
